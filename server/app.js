ef295f98b2e7b4b921f737a53721f53cbcf95d02
s-r-aman <s.r.aman.sra@gmail.com>
2018-05-20 23:19:24 +0530

    Added GraphQL related packages.

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,4 +9,6 @@ app.get('/', (req, res) => {
   res.send('Hello GraphQL');
 });
 
-app.listen(PORT, () => console.log('💻 Server is running -> 3000'));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
+
+app.listen(PORT, () => console.log('💻   Server is running -> 3000'));

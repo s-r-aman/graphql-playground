5c240818d6f6a7e0ef1a6a65eaa9c932cb404c29
s-r-aman <s.r.aman.sra@gmail.com>
2018-05-26 00:54:18 +0530

    Added functionality to addBooks component.

--- client/src/Components/AddBooks/AddBooks.js
+++ client/src/Components/AddBooks/AddBooks.js
@@ -1,51 +1,85 @@
 import React, { Component, Fragment } from 'react';
-import { gql } from 'apollo-boost';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
 
-const getAuthorsQuery = gql`
-    {
-        authors {
-            name
-            id
-        }
-    }
-`;
+import { getBooksQuery, getAuthorsQuery, addBookMutation } from './../query';
 
 class AddBook extends Component {
-    displayAuthor = () => {
-        const {
-            data: { loading, authors }
-        } = this.props;
-        if (loading) {
-            return <option disabled>Authors is loading ...</option>;
-        }
-        return (
-            <Fragment>
-                {authors.map(({ name, id }) => (
-                    <option value={id} key={id}>
-                        {name}
-                    </option>
-                ))}
-            </Fragment>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      genre: '',
+      authorId: ''
     };
-    render () {
-        return (
-            <form>
-                <label>Book name:</label>
-                <input type="text" placeholder="Enter the book name." />
-                <br />
-                <label>Genre:</label>
-                <input type="text" placeholder="Genre" />
-                <br />
-                <label>Author:</label>
-                <select name="author">
-                    <option disabled>Select the author.</option>
-                    {this.displayAuthor()}
-                </select>
-            </form>
-        );
+  }
+  displayAuthor = () => {
+    const {
+      getAuthorsQuery: { loading, authors }
+    } = this.props;
+    if (loading) {
+      return <option disabled>Authors is loading ...</option>;
     }
+    return (
+      <Fragment>
+        {authors.map(({ name, id }) => (
+          <option value={id} key={id}>
+            {name}
+          </option>
+        ))}
+      </Fragment>
+    );
+  };
+
+  submitForm = () => {
+    this.props.addBookMutation({
+      variables: this.state,
+      refetchQueries: [{ query: getBooksQuery }]
+    });
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          this.submitForm();
+        }}
+      >
+        <label>Book name:</label>
+        <input
+          type="text"
+          placeholder="Enter the book name."
+          onChange={({ target: { value: name } }) => {
+            this.setState(() => ({ name }));
+          }}
+        />
+        <br />
+        <label>Genre:</label>
+        <input
+          type="text"
+          placeholder="Genre"
+          onChange={({ target: { value: genre } }) => {
+            this.setState(() => ({ genre }));
+          }}
+        />
+        <br />
+        <label>Author:</label>
+        <select
+          name="author"
+          onChange={({ target: { value: authorId } }) => {
+            this.setState(() => ({ authorId }));
+          }}
+        >
+          <option>Select the author.</option>
+          {this.displayAuthor()}
+        </select>
+        <button type="submit">Add Book</button>
+      </form>
+    );
+  }
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+  graphql(getAuthorsQuery, { name: 'getAuthorsQuery' }),
+  graphql(addBookMutation, { name: 'addBookMutation' })
+)(AddBook);
